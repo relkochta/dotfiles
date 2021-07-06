f95bf81ea2e3e3acc99f935068600b5eed0af9ac
Ryan El Kochta <relkochta@lightn1ng.space>
2021-07-06 11:31:19 -0400

    Add -L to wallpaper.sh 'find' command
    This tells find to follow symlinks, allowing the script to work on the
    Stow-managed wallpaper collection.

--- wallpapers/.scripts/wallpaper.sh
+++ wallpapers/.scripts/wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-WALLPAPER="$(find ~/.config/wallpapers -type f -name *.png -o -name *.jpg -o -name *.jpeg | shuf -n 1)"
+WALLPAPER="$(find -L ~/.config/wallpapers -type f -name *.png -o -name *.jpg -o -name *.jpeg | shuf -n 1)"
 gsettings set org.gnome.desktop.background picture-uri "file:///$WALLPAPER"
